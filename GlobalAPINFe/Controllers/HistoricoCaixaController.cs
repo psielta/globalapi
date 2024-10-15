174b8882cf3e97754be2627342ca4ee529fbe192
mateus <psielta@gmail.com>
2024-10-15 16:04:39 -0300

    add endpoint ALL planocaixa, historicocaixa

--- GlobalAPINFe/Controllers/HistoricoCaixaController.cs
+++ GlobalAPINFe/Controllers/HistoricoCaixaController.cs
@@ -4,6 +4,7 @@ using GlobalErpData.Models;
 using GlobalErpData.Repository;
 using GlobalErpData.Repository.PagedRepositories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using X.PagedList.EF;
 
 namespace GlobalAPINFe.Controllers
@@ -33,6 +34,34 @@ namespace GlobalAPINFe.Controllers
                     return NotFound("Entities not found."); // 404 Resource not found
                 }
                 return Ok(response); // 200 OK
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error occurred while retrieving paged entities.");
+                return StatusCode(500, "An error occurred while retrieving entities. Please try again later.");
+            }
+        }
+        
+        [HttpGet("GetHistoricoCaixaPorEmpresa_ALL", Name = nameof(GetHistoricoCaixaPorEmpresa_ALL))]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> GetHistoricoCaixaPorEmpresa_ALL(int idEmpresa, [FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10)
+        {
+            try
+            {
+                var query = ((HistoricoCaixaRepository)repo).GetHistoricoCaixaPorEmpresa(idEmpresa).Result.AsQueryable();
+                if (query == null)
+                {
+                    return NotFound("Entities not found."); // 404 Resource not found
+                }
+
+                var list = await query.ToListAsync();
+
+                if (list == null || list.Count == 0)
+                {
+                    return NotFound("Entities not found."); // 404 Resource not found
+                }
+                return Ok(list); // 200 OK
             }
             catch (Exception ex)
             {
--- GlobalAPINFe/Controllers/PlanoCaixaController.cs
+++ GlobalAPINFe/Controllers/PlanoCaixaController.cs
@@ -4,6 +4,7 @@ using GlobalErpData.Models;
 using GlobalErpData.Repository;
 using GlobalErpData.Repository.PagedRepositories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using X.PagedList.EF;
 
 namespace GlobalAPINFe.Controllers
@@ -34,6 +35,33 @@ namespace GlobalAPINFe.Controllers
                     return NotFound("Entities not found."); // 404 Resource not found
                 }
                 return Ok(response); // 200 OK
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error occurred while retrieving paged entities.");
+                return StatusCode(500, "An error occurred while retrieving entities. Please try again later.");
+            }
+        }
+
+        [HttpGet("GetPlanoDeCaixaPorEmpresa_ALL", Name = nameof(GetPlanoDeCaixaPorEmpresa_ALL))]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> GetPlanoDeCaixaPorEmpresa_ALL(int idEmpresa, [FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10)
+        {
+            try
+            {
+                var query = ((PlanoCaixaRepository)repo).GetPlanoDeCaixaAsyncPorEmpresa(idEmpresa).Result.AsQueryable();
+                if (query == null)
+                {
+                    return NotFound("Entities not found."); // 404 Resource not found
+                }
+                var list = await query.ToListAsync();
+
+                if (list  == null || list.Count == 0)
+                {
+                    return NotFound("Entities not found."); // 404 Resource not found
+                }
+                return Ok(list); // 200 OK
             }
             catch (Exception ex)
             {
