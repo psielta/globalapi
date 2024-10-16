045d2c51977c67a860f451dd5670dc740894b8d2
mateus <psielta@gmail.com>
2024-10-16 19:59:31 -0300

    ___

--- GlobalErpData/Repository/PagedRepositories/ContasAPagarRepository.cs
+++ GlobalErpData/Repository/PagedRepositories/ContasAPagarRepository.cs
@@ -3,6 +3,7 @@ using GlobalErpData.Data;
 using GlobalErpData.Dto;
 using GlobalErpData.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -31,6 +32,48 @@ namespace GlobalErpData.Repository.PagedRepositories
             {
                 logger.LogError(ex, "Error occurred while retrieving all entities.");
                 return Task.FromResult(Enumerable.Empty<ContasAPagar>().AsQueryable());
+            }
+        }
+
+        public async override Task<ContasAPagar?> CreateAsync(ContasAPagarDto dto)
+        {
+            ContasAPagar entity = mapper.Map<ContasAPagar>(dto);
+            EntityEntry<ContasAPagar> added = await db.Set<ContasAPagar>().AddAsync(entity);
+            int affected = await db.SaveChangesAsync();
+            if (affected == 1)
+            {
+                if (EntityCache is null) return entity;
+                logger.LogInformation("Entity created and added to cache with ID: {Id}", entity.GetId());
+                EntityCache.AddOrUpdate(entity.GetId(), entity, UpdateCache);
+
+                return await db.Set<ContasAPagar>().Include(e => e.Fornecedor)
+                    .Include(e => e.HistoricoCaixa).ThenInclude(h => h.PlanoDeCaixa)
+                    .FirstOrDefaultAsync(e => e.Id == entity.Id);
+            }
+            else
+            {
+                logger.LogWarning("Failed to create entity from DTO.");
+                return null;
+            }
+        }
+        public async override Task<ContasAPagar?> UpdateAsync(int id, ContasAPagarDto dto)
+        {
+            ContasAPagar entity = mapper.Map<ContasAPagar>(dto);
+            entity.GetType().GetProperty(entity.GetKeyName())?.SetValue(entity, id);
+            db.Set<ContasAPagar>().Update(entity);
+            int affected = await db.SaveChangesAsync();
+            if (affected == 1)
+            {
+                logger.LogInformation("Entity updated with ID: {Id}", id);
+                UpdateCache(id, entity);
+                return await db.Set<ContasAPagar>().Include(e => e.Fornecedor)
+                    .Include(e => e.HistoricoCaixa).ThenInclude(h => h.PlanoDeCaixa)
+                    .FirstOrDefaultAsync(e => e.Id == id);
+            }
+            else
+            {
+                logger.LogWarning("Failed to update entity with ID: {Id}", id);
+                return null;
             }
         }
     }
