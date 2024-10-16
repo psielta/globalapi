927ca729de69ca8646385589c57a7bb3843e76d8
mateus <psielta@gmail.com>
2024-10-16 17:42:38 -0300

    ___

--- GlobalAPINFe/Controllers/FormaPagtController.cs
+++ GlobalAPINFe/Controllers/FormaPagtController.cs
@@ -4,6 +4,7 @@ using GlobalErpData.Models;
 using GlobalErpData.Repository;
 using GlobalErpData.Repository.PagedRepositories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using X.PagedList.EF;
 
 namespace GlobalAPINFe.Controllers
@@ -42,6 +43,31 @@ namespace GlobalAPINFe.Controllers
             }
         }
 
+        [HttpGet("GetFormaPagtPorEmpresa", Name = nameof(GetFormaPagtPorEmpresa))]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> GetFormaPagtPorEmpresa_ALL(int idEmpresa, [FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10)
+        {
+            try
+            {
+                var query = ((FormaPagtRepository)repo).GetFormaPagtAsyncPorEmpresa(idEmpresa).Result.AsQueryable();
+                if (query == null)
+                {
+                    return NotFound("Entities not found."); // 404 Resource not found
+                }
+                var list = await query.AsNoTracking().ToListAsync();
 
+                if (list == null || list.Count == 0)
+                {
+                    return NotFound("Entities not found."); // 404 Resource not found
+                }
+                return Ok(list); // 200 OK
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error occurred while retrieving paged entities.");
+                return StatusCode(500, "An error occurred while retrieving entities. Please try again later.");
+            }
+        }
     }
 }
