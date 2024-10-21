19467529c48b0a41ed75dec66f70a50a1aaddc27
mateus <psielta@gmail.com>
2024-10-21 20:48:24 -0300

    _

---
+++ GlobalAPI_ACBrNFe/Controllers/RegNfeController.cs
@@ -0,0 +1,41 @@
+﻿using GlobalAPI_ACBrNFe.Models;
+using GlobalErpData.Data;
+using GlobalErpData.Dto;
+using GlobalErpData.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GlobalAPI_ACBrNFe.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegNfeController : Controller
+    {
+        protected GlobalErpFiscalBaseContext db;
+        protected readonly ILogger<ImpNfeController> logger;
+        private string ENDPOINT_POST_FORNECECOR;
+        public RegNfeController(GlobalErpFiscalBaseContext db, ILogger<ImpNfeController> logger)
+        {
+            this.db = db;
+            this.logger = logger;
+            ENDPOINT_POST_FORNECECOR = Constants.URL_API_NFE + "/api/Fornecedor";
+        }
+
+        [HttpPost("Registrar/{idEmpresa}/{chaveAcesso}", Name = nameof(Registrar))]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
+        public async Task<ActionResult<Entrada>> Registrar([FromBody] ImpNFeTemp impNFeTemp, int idEmpresa, string chaveAcesso)
+        {
+            if (idEmpresa == 0 || string.IsNullOrEmpty(chaveAcesso)||  chaveAcesso.Length < 44 )
+            {
+                return BadRequest(new ErrorMessage(500, "Is missing parameters"));
+            }
+            if (impNFeTemp == null)
+            {
+                return BadRequest(new ErrorMessage(500, "Is missing body"));
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
