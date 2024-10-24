ff28fc57edd591d55abf4a14de8707136f3a6f75
mateus <psielta@gmail.com>
2024-10-23 21:26:04 -0300

    _

--- GlobalAPI_ACBrNFe/Controllers/RegNfeController.cs
+++ GlobalAPI_ACBrNFe/Controllers/RegNfeController.cs
@@ -2,7 +2,10 @@
 using GlobalErpData.Data;
 using GlobalErpData.Dto;
 using GlobalErpData.Models;
+using GlobalLib.Strings;
+using GlobalLib.Utils;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GlobalAPI_ACBrNFe.Controllers
 {
@@ -20,26 +23,201 @@ namespace GlobalAPI_ACBrNFe.Controllers
             ENDPOINT_POST_FORNECECOR = Constants.URL_API_NFE + "/api/Fornecedor";
         }
 
-        [HttpPost("Registrar/{idEmpresa}/{chaveAcesso}", Name = nameof(Registrar))]
+        [HttpPost("Registrar/{idEmpresa}/{chaveAcesso}/{cdPlanoEstoque}", Name = nameof(Registrar))]
         [ProducesResponseType(200)]
         [ProducesResponseType(400)]
         [ProducesResponseType(500)]
-        public async Task<ActionResult<Entrada>> Registrar([FromBody] ImpNFeTemp impNFeTemp, int idEmpresa, string chaveAcesso)
+        public async Task<ActionResult<Entrada>> Registrar([FromBody] ImpNFeTemp impNFeTemp, int idEmpresa, int cdPlanoEstoque, string chaveAcesso)
         {
-            if (idEmpresa == 0 || string.IsNullOrEmpty(chaveAcesso)||  chaveAcesso.Length < 44 )
+            #region Validações
+            if (idEmpresa == 0 || string.IsNullOrEmpty(chaveAcesso) || chaveAcesso.Length < 44)
             {
+                logger.LogError($"Erro ao buscar NFe ({chaveAcesso}).");
                 return BadRequest(new ErrorMessage(500, "Is missing parameters"));
             }
             if (impNFeTemp == null)
             {
+                logger.LogError($"Erro ao buscar NFe ({chaveAcesso}).");
                 return BadRequest(new ErrorMessage(500, "Is missing body"));
             }
             if (impNFeTemp.amarracoes == null)
             {
+                logger.LogError($"Erro ao buscar amarrações ({chaveAcesso}).");
                 return BadRequest(new ErrorMessage(500, "Is missing NFe"));
             }
+            #endregion
+            #region Busca Fornecedor
+            int IdFornecedor = 0;
+            Fornecedor? fornecedor = null;
+
+            try
+            {
+                IdFornecedor = impNFeTemp.amarracoes.FirstOrDefault().CdForn;
+                fornecedor = await db.Fornecedors.FindAsync(IdFornecedor);
+                if (IdFornecedor <= 0 || fornecedor == null)
+                {
+                    return BadRequest(new ErrorMessage(500, "Is missing Fornecedor"));
+                }
+            }
+            catch
+            {
+                logger.LogError($"Erro ao buscar fornecedor ({IdFornecedor}, {chaveAcesso}).");
+                return BadRequest(new ErrorMessage(500, "Is missing Fornecedor"));
+            }
+            #endregion
+            #region Busca Transportadora
+            Transportadora? transportadora;
+            try
+            {
+                transportadora = await ImportarTransportadora(impNFeTemp, idEmpresa);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, $"Erro importação de transportadora ({chaveAcesso}).");
+                return BadRequest(new ErrorMessage(500, "Error importing Transp"));
+            }
+            #endregion
+            #region Entrada
+            try
+            {
+                Entrada? entrada = await ImportarEntrada(IdFornecedor, transportadora, impNFeTemp, idEmpresa, cdPlanoEstoque);
+                if (entrada == null)
+                {
+                    logger.LogError($"Erro ao importar entrada ({chaveAcesso}).");
+                    return BadRequest(new ErrorMessage(500, "Error importing Entrada"));
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, $"Erro ao importar entrada ({chaveAcesso}).");
+                return BadRequest(new ErrorMessage(500, "Error importing Entrada"));
+            }
+            #endregion
 
             throw new NotImplementedException();
         }
+
+        private async Task<Entrada?> ImportarEntrada(int idFornecedor, Transportadora? transportadora, ImpNFeTemp impNFeTemp, int idEmpresa, int cdPlanoEstoque)
+        {
+            Entrada? entrada = db.Entradas.FromSqlRaw($@"
+                SELECT 
+                    * 
+                FROM 
+                    public.entradas 
+                WHERE 
+                    cd_empresa = {idEmpresa} 
+                    AND cd_chave_nfe = '{impNFeTemp.impcabnfe?.ChNfe}'").FirstOrDefault();
+
+            if (entrada == null)
+            {
+                DateOnly data = DateUtils.DateTimeToDateOnly(impNFeTemp.impcabnfe?.DtEmissao ?? DateTime.Now);
+                entrada.Data = data;
+                if (impNFeTemp.impcabnfe.DtSaida == new DateTime(1899, 12, 30))
+                {
+                    entrada.DtSaida = data;
+                }
+                else
+                {
+                    entrada.DtSaida = DateUtils.DateTimeToDateOnly(impNFeTemp.impcabnfe.DtSaida ?? DateTime.Now);
+                }
+                entrada.CdForn = idFornecedor;
+                entrada.CdEmpresa = idEmpresa;
+                if (transportadora != null)
+                {
+                    entrada.Transp = transportadora.CdTransportadora;
+                }
+                entrada.CdTipoNf = "55";
+                entrada.CdChaveNfe = impNFeTemp.impcabnfe.ChNfe;
+                entrada.SerieNf = impNFeTemp.impcabnfe.Serie;
+                entrada.NrNf = impNFeTemp.impcabnfe.NrNf;
+                entrada.ModeloNf = impNFeTemp.impcabnfe.Modelo;
+                entrada.TxtObs = impNFeTemp.impcabnfe.InfObs;
+                entrada.TPag = impNFeTemp.impcabnfe.TPag;
+
+                decimal vIcmsD = 0;
+                try
+                {
+                    vIcmsD = Convert.ToDecimal(impNFeTemp.imptotalnfe.Vicmsdeson ?? "0");
+                }
+                catch
+                {
+                    vIcmsD = 0;
+                }
+                entrada.VIcmsDeson = vIcmsD;
+
+                if ((impNFeTemp.impcabnfe.TpPagt ?? "").Equals("2"))
+                    impNFeTemp.impcabnfe.TpPagt = "P";
+                else impNFeTemp.impcabnfe.TpPagt = "V";
+
+                int tpFrete;
+                try
+                {
+                    tpFrete = Convert.ToInt32(impNFeTemp.impcabnfe.TpFrete);
+                }
+                catch (Exception)
+                {
+                    throw new Exception("Erro ao converter frete (tpFrete)");
+                }
+                switch (tpFrete)
+                {
+                    case 0:
+                        entrada.TpFrete = 1;
+                        break;
+                    case 1:
+                        entrada.TpFrete = 2;
+                        break;
+                    case 3:
+                        entrada.TpFrete = 0;
+                        break;
+                    case 4:
+                        entrada.TpFrete = 0;
+                        break;
+                }
+                entrada.VlFrete = Convert.ToDecimal(impNFeTemp.imptotalnfe.IcmsFrete ?? "0");
+                entrada.VlSeguro = Convert.ToDecimal(impNFeTemp.imptotalnfe.IcmsSeg ?? "0");
+                entrada.VlOutras = Convert.ToDecimal(impNFeTemp.imptotalnfe.IcmsOutros ?? "0");
+                entrada.VlDescontoNf = Convert.ToDecimal(impNFeTemp.imptotalnfe.IcmsDesc ?? "0");
+                entrada.VlStNf = Convert.ToDecimal(impNFeTemp.imptotalnfe.IcmsSt ?? "0");
+                entrada.CdGrupoEstoque = cdPlanoEstoque;
+
+                db.Entradas.Add(entrada);
+                await db.SaveChangesAsync();
+            }
+            return entrada;
+        }
+
+        private async Task<Transportadora?> ImportarTransportadora(ImpNFeTemp impNFeTemp, int idEmpresa)
+        {
+            if (string.IsNullOrEmpty(impNFeTemp?.impcabnfe?.CnpjTransp))
+            {
+                return null;
+            }
+            string cnpj = UtlStrings.OnlyInteger(impNFeTemp?.impcabnfe?.CnpjTransp);
+            string SQLtransp = $@"
+                        SELECT 
+                          *
+                        FROM 
+                          public.transportadora
+                        WHERE 
+                          id_empresa = {idEmpresa} AND
+                          regexp_replace(cd_cnpj, '[^0-9]', '', 'g') = '{cnpj}'";
+
+            Transportadora? transportadora = await db.Transportadoras.FromSqlRaw(SQLtransp).FirstOrDefaultAsync();
+            if (transportadora == null)
+            {
+                transportadora = new Transportadora();
+                transportadora.NmTransportadora = impNFeTemp.impcabnfe?.NomeTransp;
+                transportadora.CdCnpj = cnpj;
+                transportadora.Numero = 0;
+                transportadora.NmEndereco = impNFeTemp.impcabnfe?.EndTransp;
+                transportadora.CdCidade = impNFeTemp.impcabnfe?.CidadeTransp;
+                transportadora.IdEmpresa = idEmpresa;
+
+                db.Transportadoras.Add(transportadora);
+                await db.SaveChangesAsync();
+            }
+            return transportadora;
+        }
+
     }
 }
---
+++ GlobalLib/Utils/DateUtils.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalLib.Utils
+{
+    public static class DateUtils
+    {
+        /// <summary>
+        /// Converte um objeto DateTime em um objeto DateOnly.
+        /// </summary>
+        /// <param name="dateTime">O objeto DateTime a ser convertido.</param>
+        /// <returns>O objeto DateOnly convertido.</returns>
+        public static DateOnly DateTimeToDateOnly(DateTime dateTime)
+        {
+            return DateOnly.FromDateTime(dateTime);
+        }
+
+        /// <summary>
+        /// Converte um objeto DateOnly em um objeto DateTime.
+        /// </summary>
+        /// <param name="dateOnly">O objeto DateOnly a ser convertido.</param>
+        /// <returns>O objeto DateTime convertido.</returns>
+        public static DateTime DateOnlyToDateTime(DateOnly dateOnly)
+        {
+            return dateOnly.ToDateTime(TimeOnly.MinValue);
+        }
+
+        /// <summary>
+        /// Verifica se uma data é um fim de semana (sábado ou domingo).
+        /// </summary>
+        /// <param name="dateOnly">O objeto DateOnly a ser verificado.</param>
+        /// <returns>True se a data for um fim de semana, caso contrário, false.</returns>
+        public static bool IsWeekend(DateOnly dateOnly)
+        {
+            DayOfWeek day = dateOnly.DayOfWeek;
+            return day == DayOfWeek.Saturday || day == DayOfWeek.Sunday;
+        }
+
+        /// <summary>
+        /// Verifica se uma data está dentro de um intervalo de datas.
+        /// </summary>
+        /// <param name="date">O objeto DateOnly a ser verificado.</param>
+        /// <param name="startDate">A data de início do intervalo.</param>
+        /// <param name="endDate">A data de término do intervalo.</param>
+        /// <returns>True se a data estiver dentro do intervalo, caso contrário, false.</returns>
+        public static bool IsWithinRange(DateOnly date, DateOnly startDate, DateOnly endDate)
+        {
+            return date >= startDate && date <= endDate;
+        }
+
+        /// <summary>
+        /// Calcula o número de dias entre duas datas.
+        /// </summary>
+        /// <param name="startDate">A data de início.</param>
+        /// <param name="endDate">A data de término.</param>
+        /// <returns>O número de dias entre as duas datas.</returns>
+        public static int DaysBetween(DateOnly startDate, DateOnly endDate)
+        {
+            return Math.Abs((endDate.ToDateTime(TimeOnly.MinValue) - startDate.ToDateTime(TimeOnly.MinValue)).Days);
+        }
+
+        /// <summary>
+        /// Retorna a data atual como um objeto DateOnly.
+        /// </summary>
+        /// <returns>A data atual como um objeto DateOnly.</returns>
+        public static DateOnly Today()
+        {
+            return DateOnly.FromDateTime(DateTime.Now);
+        }
+
+        /// <summary>
+        /// Adiciona um número especificado de dias a um objeto DateOnly e retorna o resultado.
+        /// </summary>
+        /// <param name="date">O objeto DateOnly ao qual adicionar dias.</param>
+        /// <param name="days">O número de dias a serem adicionados.</param>
+        /// <returns>O objeto DateOnly com os dias adicionados.</returns>
+        public static DateOnly AddDays(DateOnly date, int days)
+        {
+            return date.AddDays(days);
+        }
+
+        /// <summary>
+        /// Converte uma string em formato de data em um objeto DateOnly.
+        /// </summary>
+        /// <param name="dateString">A string em formato de data a ser convertida.</param>
+        /// <returns>O objeto DateOnly convertido.</returns>
+        public static DateOnly ParseDateOnly(string dateString)
+        {
+            return DateOnly.Parse(dateString);
+        }
+
+        /// <summary>
+        /// Tenta converter uma string em formato de data em um objeto DateOnly, retornando um booleano indicando o sucesso.
+        /// </summary>
+        /// <param name="dateString">A string em formato de data a ser convertida.</param>
+        /// <param name="date">O objeto DateOnly convertido.</param>
+        /// <returns>True se a conversão for bem-sucedida, caso contrário, false.</returns>
+        public static bool TryParseDateOnly(string dateString, out DateOnly date)
+        {
+            return DateOnly.TryParse(dateString, out date);
+        }
+    }
+
+}
