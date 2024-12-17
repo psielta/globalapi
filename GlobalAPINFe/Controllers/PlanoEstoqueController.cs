21b2f71868bf129f1b0226c497116e7279f3bb46
Mateus <psielta@gmail.com>
2024-12-17 10:53:41 -0300

    ___

--- GlobalAPINFe/Controllers/PlanoEstoqueController.cs
+++ GlobalAPINFe/Controllers/PlanoEstoqueController.cs
@@ -144,5 +144,47 @@ namespace GlobalAPINFe.Controllers
                 return StatusCode(500, "An error occurred while retrieving entities. Please try again later.");
             }
         }
+        
+        [HttpGet("GetPlanoEstoqueFiscalPorEmpresa", Name = nameof(GetPlanoEstoqueFiscalPorEmpresa))]
+        [ProducesResponseType(typeof(PagedResponse<PlanoEstoque>), 200)]
+        [ProducesResponseType(404)]
+        public async Task<ActionResult<PagedResponse<PlanoEstoque>>> GetPlanoEstoqueFiscalPorEmpresa(
+            int idEmpresa,
+            [FromQuery] int pageNumber = 1,
+            [FromQuery] int pageSize = 10)
+        {
+            try
+            {
+                var query = ((PlanoEstoquePagedRepository)repo).GetPlanoEstoquePorEmpresa(idEmpresa).Result.AsQueryable();
+
+                if (query == null)
+                {
+                    return NotFound("Entities not found.");
+                }
+
+                query = query.Where(p => p.EFiscal == true);
+
+                var filteredQuery = query.AsEnumerable();
+
+
+
+                filteredQuery = filteredQuery.OrderByDescending(p => p.CdPlano);
+
+                var pagedList = filteredQuery.ToPagedList(pageNumber, pageSize);
+                var response = new PagedResponse<PlanoEstoque>(pagedList);
+
+                if (response.Items == null || response.Items.Count == 0)
+                {
+                    return NotFound("Entities not found."); // 404 Resource not found
+                }
+
+                return Ok(response); // 200 OK
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error occurred while retrieving paged entities.");
+                return StatusCode(500, "An error occurred while retrieving entities. Please try again later.");
+            }
+        }
     }
 }
